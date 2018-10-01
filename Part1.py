71a4f38e2e2d771b937e6f6243320d3f22df512e
Henry Warren <henrysdev@gmail.com>
2018-09-30 19:33:16 -0500

    implemented all random distribution functions

--- Part1.py
+++ Part1.py
@@ -16,10 +16,36 @@ OUTPUT:
 •	An example output file will be provided on Canvas.
 """
 import random
+import math
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
 
-def prettyprint(mylist):
-    for row in mylist:
-        print(row)
+
+def uniform(high):
+    return random.uniform(0, high)
+
+def skewed(high):
+    x = 1.0 - random.random()
+    return high + (-1 * high) * math.sqrt(x)
+
+def two_tiered(high, cutoff=0.1, first_portion=0.5):
+    x = random.random()
+    if x < first_portion:
+        return random.uniform(0, math.floor(N * cutoff))
+    else:
+        return random.uniform(math.floor(N * cutoff), N)
+
+def triangular(high, mode):
+    x = random.random()
+    low = 0
+    mode = 0.5 if mode is None else (mode - low) / (high - low)
+    if x > mode:
+        x = 1.0 - x
+        mode = 1.0 - mode
+        low, high = high, low
+    return low + (high - low) * math.sqrt(x * mode)
+
 
 def schedule_confs(N, S, K):
     # Uniform
@@ -33,4 +59,16 @@ def schedule_confs(N, S, K):
 
     return attendees
 
-prettyprint(schedule_confs(100, 1000, 10))
+
+N = 10
+trials = 2000
+
+y_pos = list(range(1,N+1))
+
+py_performance = [0] * N
+for i in range(trials):
+    result = two_tiered(N) # uniform(N) # triangular(N,N//2)
+    py_performance[int(result)] += 1
+plt.bar(y_pos, py_performance, align="center", alpha=0.5)
+
+plt.show()
